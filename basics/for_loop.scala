6cdf1e06d64f4b025d8426ef5b2405c860c57dde
Devendra Kumar Singh <58590533+devendra631997@users.noreply.github.com>
2024-05-25 07:19:12 +0000

    for loop

---
+++ basics/for_loop.scala
@@ -0,0 +1,7 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+    for (i ← 10 to 0 by -1) {
+      println(i)
+    }
+  }
+}
--- basics/loop.scala
+++
@@ -1,5 +0,0 @@
-var x = 0
-
-for(x <- 1 to 20){
-    println(x)
-}
